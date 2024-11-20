b59dee435f81adac5eb6f606c1ee6403c8b36ee0
Daniel Gimaev <143883330+DanielGim-Hub@users.noreply.github.com>
2024-11-20 17:36:02 +0300

    UserRepository.java

--- src/UserRepository.java
+++ src/UserRepository.java
@@ -5,6 +5,6 @@ public interface UserRepository extends CrudRepository<User> {
     List<User> findAllByEmail(String email);
     List<User> findAllByPhoneNumber(String phoneNumber);
     List<User> findAllByCity(String city);
-    List<User> findAllByGroupNumber(String groupNumber); // Новый метод
+    List<User> findAllByGroupNumber(String groupNumber);
 }
 
